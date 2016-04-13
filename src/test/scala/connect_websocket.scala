2f4095e7957b1988a4536a998871d58ecfd725c8
Richard Clamp <richardc@unixbeard.net>
2016-04-13 13:06:42 +0100

    Add a pcp class with simplified helpers
    Not quite working byte[] isn't getting recognised as scalas Array[Byte]

--- src/test/scala/connect_websocket.scala
+++ src/test/scala/connect_websocket.scala
@@ -6,11 +6,11 @@ class ConnectWebsocketSimulation extends Simulation {
   val conf = http.wsBaseURL("wss://localhost:8142")
 
   val scn = scenario("ConnectWebsocket")
-      .exec(ws("Connect WS").open("/pcp"))
-      .exec(ws("Association")
-        .sendText("hello I guess")
-        .check(wsAwait.within(30).until(1)))
-      .exec(ws("Close WS").close)
+    .exec(ws("Connect WS").open("/pcp"))
+    .exec(ws("Association")
+      .sendBytes(pcp.associate_request("pcp://client01.example.com/test01"))
+      .check(wsAwait.within(30).until(1)))
+    .exec(ws("Close WS").close)
 
   setUp(scn.inject(atOnceUsers(1))).protocols(conf)
 }
